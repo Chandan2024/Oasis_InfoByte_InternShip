805462dedce33b01906ff15291166964d97600b7
Chandan2024 <chandanw03@gmail.com>
2023-09-13 21:05:14 +0530

    Final Commit

--- Task2.java
+++ Task2.java
@@ -4,8 +4,8 @@ class Task2 {
     private static final Scanner z = new Scanner(System.in);
     private static final Random r = new Random();
 
-    public static void L1(int rnum, int attempt, int n, int count) {
-        System.out.println("Attempt Allowed: " + attempt +"\nLimit: 1-10"+ "\n--------------------");
+    public static void L(int rnum, int attempt, int n, int count) {
+        System.out.println("Attempt Allowed: " + attempt + "\nLimit: 1-10" + "\n--------------------");
         System.out.print("\nEnter a number:");
         n = z.nextInt();
         count++;
@@ -17,51 +17,15 @@ class Task2 {
             n = z.nextInt();
             count++;
         }
-        if (n == rnum)
-            System.out.println("\n---------------------------------\n|Attempts Took: " + count + " | Score: "
-                    + (attempt - count) * 2 + "/" + attempt * 2 + "|\n---------------------------------");
-        else
-            System.out.println("Bhag BSDK");
-    }
-
-    public static void L2(int rnum, int attempt, int n, int count) {
-        System.out.println("Attempt Allowed: " + attempt + "\nLimit: 1-50"+"\n--------------------");
-        System.out.print("\nEnter a number:");
-        n = z.nextInt();
-        count++;
-        while (n != rnum && count != attempt) {
-            if (n > rnum)
-                System.out.print("Lower Number:  ");
-            else
-                System.out.print("Higher Number: ");
-            n = z.nextInt();
-            count++;
+        if (n == rnum) {
+            int score = ((attempt - count) * 2) + 2;
+            System.out.println("\n-----------------------------------\n| Attempts Took: " + count + " | Score: "
+                    + score + "/" + attempt * 2 + " |\n-----------------------------------");
+        } else {
+            int score = ((attempt - count) * 2) + 2;
+            System.out.println("\n-----------------------------------\n| Attempts Took: " + count + " | Score: "
+                    + score + "/" + attempt * 2 + " |\n-----------------------------------");
         }
-        if (n == rnum)
-            System.out.println("\n---------------------------------\n|Attempts Took: " + count + " | Score: "
-                    + (attempt - count) * 2 + "/" + attempt * 2 + "|\n---------------------------------");
-        else
-            System.out.println("Bhag BSDK");
-    }
-
-    public static void L3(int rnum, int attempt, int n, int count) {
-        System.out.println("Attempt Allowed: " + attempt + "\nLimit: 1-100"+"\n--------------------");
-        System.out.print("\nEnter a number:");
-        n = z.nextInt();
-        count++;
-        while (n != rnum && count != attempt ) {
-            if (n > rnum)
-                System.out.print("Lower Number:  ");
-            else
-                System.out.print("Higher Number: ");
-            n = z.nextInt();
-            count++;
-        }
-        if (n == rnum)
-            System.out.println("\n---------------------------------\n|Attempts Took: " + count + " | Score: "
-                    + (attempt - count) * 2 + "/" + attempt * 2 + "|\n---------------------------------");
-        else
-            System.out.println("Bhag BSDK");
     }
 
     public static void main(String args[]) {
@@ -81,19 +45,19 @@ class Task2 {
             rnum = r.nextInt(10) + 1;
             attempt = 10;
             System.out.println("Level: Easy");
-            L1(rnum, attempt, n, count);
+            L(rnum, attempt, n, count);
         }
         if (lvl == 2) {
             rnum = r.nextInt(50) + 1;
             attempt = 10;
             System.out.println("Level: Medium");
-            L2(rnum, attempt, n, count);
+            L(rnum, attempt, n, count);
         }
         if (lvl == 3) {
             rnum = r.nextInt(100) + 1;
             attempt = 10;
             System.out.println("Level: Hard");
-            L3(rnum, attempt, n, count);
+            L(rnum, attempt, n, count);
         }
         z.close();
     }
